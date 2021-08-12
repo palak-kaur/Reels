404f6c9a69fe156084eb2061238822d7226a9333
unknown <palakkaur0308@gmail.com>
2021-08-12 13:34:34 +0530

    signup and login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Login from "./Components/Login";
+import Feed from "./Components/Feed";
+import Profile from "./Components/Profile";
+import Signup from "./Components/Signup";
+import AuthProvider from "./Context/AuthProvider";
+import { Switch, Route, Redirect } from "react-router-dom";
+let isAuthenticated = false;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <h1>Hello Reels</h1> */}
+      {/* <Login></Login> */}
+      {/* <Todo></Todo> */}
+      {/* 3. */}
+      <AuthProvider>
+        <Switch>
+          <Route path="/login" component={Login}></Route>
+          <Route path="/signup" component={Signup}></Route>
+          <Route path="/feed" component={Feed}></Route>
+          <Route path="/profile" component={Profile}></Route>
+          <Redirect path="/" to="/feed"></Redirect>
+        </Switch>
+      </AuthProvider>
+    </>
   );
+}
+function ProtectedRoute(props) {
+  console.log(props);
+  let Component = props.abc;
+  return (<Route {...props} render={(props) => {
+    console.log(isAuthenticated);
+    return (isAuthenticated == true ? <Component {...props} ></Component> : <Redirect to="/login"></Redirect>
+    )
+  }}></Route>
+
+  )
 }
 
 export default App;
---
+++ src/secrets.js
@@ -0,0 +1,10 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyCXORf2o0rBqTfDKQoQwnNeVlffVH9D720",
+    authDomain: "reels-project.firebaseapp.com",
+    projectId: "reels-project",
+    storageBucket: "reels-project.appspot.com",
+    messagingSenderId: "430698386729",
+    appId: "1:430698386729:web:73b988dfdfeeb10fc89338"
+};
+
+export default firebaseConfig;
